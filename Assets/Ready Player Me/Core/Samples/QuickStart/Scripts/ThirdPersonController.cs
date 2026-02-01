ff04815cf0b426f2cc54436ccb9f44c15775f0ce
Martins Philip <142056130+manumitgame@users.noreply.github.com>
2026-02-01 17:38:27 +0100

    commit

--- Assets/Ready Player Me/Core/Samples/QuickStart/Scripts/ThirdPersonController.cs
+++ Assets/Ready Player Me/Core/Samples/QuickStart/Scripts/ThirdPersonController.cs
@@ -1,201 +1,100 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace ReadyPlayerMe.Samples.QuickStart
 {
-    [RequireComponent(typeof(ThirdPersonMovement), typeof(PlayerInput))]
+    [RequireComponent(typeof(ThirdPersonMovement),typeof(PlayerInput))]
     public class ThirdPersonController : MonoBehaviour
     {
         private const float FALL_TIMEOUT = 0.15f;
-
+            
         private static readonly int MoveSpeedHash = Animator.StringToHash("MoveSpeed");
         private static readonly int JumpHash = Animator.StringToHash("JumpTrigger");
         private static readonly int FreeFallHash = Animator.StringToHash("FreeFall");
         private static readonly int IsGroundedHash = Animator.StringToHash("IsGrounded");
-        private static readonly int IsEquippedHash = Animator.StringToHash("IsEquipped");
-        private static readonly int IsAimingHash = Animator.StringToHash("IsAiming");
-
-        [Header("Keys")]
-        [SerializeField] private KeyCode equipKey = KeyCode.E;
-        [SerializeField] private KeyCode shootKey = KeyCode.Return;
-
-        [Header("Input")]
-        [SerializeField] private bool inputEnabled = true;
-
-        public GameObject unequippedGun;
-        public GameObject equippedGun;
-
+        
+        private Transform playerCamera;
         private Animator animator;
+        private Vector2 inputVector;
+        private Vector3 moveVector;
         private GameObject avatar;
-
-        private ThirdPersonMovement movement;
+        private ThirdPersonMovement thirdPersonMovement;
         private PlayerInput playerInput;
-        private CameraFollow cameraFollow;
-
+        
         private float fallTimeoutDelta;
-
-        private bool isEquipped;
-        private bool isAiming;
+        
+        [SerializeField][Tooltip("Useful to toggle input detection in editor")]
+        private bool inputEnabled = true;
         private bool isInitialized;
-
-        // ===================== INIT =====================
 
         private void Init()
         {
-            movement = GetComponent<ThirdPersonMovement>();
+            thirdPersonMovement = GetComponent<ThirdPersonMovement>();
             playerInput = GetComponent<PlayerInput>();
             playerInput.OnJumpPress += OnJump;
-
-            cameraFollow = FindObjectOfType<CameraFollow>();
-            unequippedGun.SetActive(true);
-            equippedGun.SetActive(false);
             isInitialized = true;
         }
 
-        public void Setup(GameObject target, RuntimeAnimatorController controller)
+        public void Setup(GameObject target, RuntimeAnimatorController runtimeAnimatorController)
         {
             if (!isInitialized)
+            {
                 Init();
-
+            }
+            
             avatar = target;
-
-            movement.Setup(avatar);
-
+            thirdPersonMovement.Setup(avatar);
             animator = avatar.GetComponent<Animator>();
-            animator.runtimeAnimatorController = controller;
+            animator.runtimeAnimatorController = runtimeAnimatorController;
             animator.applyRootMotion = false;
-
-            SetEquipped(false);
-            SetAiming(false);
+            
         }
-
-        // ===================== UPDATE =====================
-
+        
         private void Update()
         {
             if (avatar == null)
+            {
                 return;
-
-            HandleEquipInput();
-            HandleAimingInput();
-
+            }
             if (inputEnabled)
             {
                 playerInput.CheckInput();
-
-                movement.Move(
-                    playerInput.AxisHorizontal,
-                    playerInput.AxisVertical
-                );
-
-                movement.SetIsRunning(playerInput.IsHoldingLeftShift);
+                var xAxisInput = playerInput.AxisHorizontal;
+                var yAxisInput = playerInput.AxisVertical;
+                thirdPersonMovement.Move(xAxisInput, yAxisInput);
+                thirdPersonMovement.SetIsRunning(playerInput.IsHoldingLeftShift);
             }
-
-            
-
             UpdateAnimator();
         }
 
-        // ===================== INPUT =====================
-
-        private void HandleEquipInput()
-        {
-            if (Input.GetKeyDown(equipKey))
-            {
-                SetEquipped(!isEquipped);
-                
-            }
-            if(isEquipped)
-            {
-                unequippedGun.SetActive(false);
-                equippedGun.SetActive(true);
-            }else
-            {
-                unequippedGun.SetActive(true);
-                equippedGun.SetActive(false);
-            }
-        }
-
-        private void HandleAimingInput()
-        {
-            bool aimingNow = Input.GetKey(shootKey);
-
-            // 🔥 Shooting ALWAYS forces equipped animations
-            if (aimingNow)
-            {
-                if (!isEquipped)
-                    SetEquipped(true);
-
-                SetAiming(true);
-            }
-            else
-            {
-                SetAiming(false);
-            }
-        }
-
-        // ===================== STATE =====================
-
-        private void SetEquipped(bool equipped)
-        {
-            isEquipped = equipped;
-
-            movement.SetEquipped(equipped);
-
-            animator.SetBool(IsEquippedHash, equipped);
-
-            int equippedLayer = animator.GetLayerIndex("EquippedLayer");
-            if (equippedLayer != -1)
-            {
-                animator.SetLayerWeight(equippedLayer, equipped ? 1f : 0f);
-            }
-           
-            // If unequipped manually, cancel aiming
-            if (!equipped)
-                SetAiming(false);
-        }
-
-        private void SetAiming(bool aiming)
-        {
-            if (isAiming == aiming)
-                return;
-
-            isAiming = aiming;
-
-            movement.SetAiming(aiming);
-
-            animator.SetBool(IsAimingHash, aiming);
-
-            if (cameraFollow != null)
-                cameraFollow.SetCameraLocked(aiming);
-        }
-
-        // ===================== ANIMATION =====================
-
         private void UpdateAnimator()
         {
-            bool grounded = movement.IsGrounded();
-
-            animator.SetFloat(MoveSpeedHash, movement.CurrentMoveSpeed);
-            animator.SetBool(IsGroundedHash, grounded);
-
-            if (grounded)
+            var isGrounded = thirdPersonMovement.IsGrounded();
+            animator.SetFloat(MoveSpeedHash, thirdPersonMovement.CurrentMoveSpeed);
+            animator.SetBool(IsGroundedHash, isGrounded);
+            if (isGrounded)
             {
                 fallTimeoutDelta = FALL_TIMEOUT;
                 animator.SetBool(FreeFallHash, false);
             }
             else
             {
-                if (fallTimeoutDelta > 0f)
+                if (fallTimeoutDelta >= 0.0f)
+                {
                     fallTimeoutDelta -= Time.deltaTime;
+                }
                 else
+                {
                     animator.SetBool(FreeFallHash, true);
+                }
             }
         }
 
         private void OnJump()
         {
-            if (movement.TryJump())
+            if (thirdPersonMovement.TryJump())
+            {
                 animator.SetTrigger(JumpHash);
+            }
         }
     }
 }
--- Assets/Ready Player Me/Core/Samples/QuickStart/Scripts/ThirdPersonMovement.cs
+++ Assets/Ready Player Me/Core/Samples/QuickStart/Scripts/ThirdPersonMovement.cs
@@ -1,173 +1,112 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace ReadyPlayerMe.Samples.QuickStart
 {
     [RequireComponent(typeof(CharacterController), typeof(GroundCheck))]
     public class ThirdPersonMovement : MonoBehaviour
     {
-        private const float TURN_SMOOTH_TIME = 0.08f;
+        private const float TURN_SMOOTH_TIME = 0.05f;
 
-        [Header("Movement")]
-        [SerializeField] private float walkSpeed = 4f;
-        [SerializeField] private float runSpeed = 7.5f;
-        [SerializeField] private float airControlMultiplier = 0.6f;
-
-        [Header("Jump & Gravity")]
-        [SerializeField] private float gravity = -25f;
-        [SerializeField] private float jumpHeight = 3f;
-
-        [Header("References")]
-        [SerializeField] private CameraFollow cameraFollow;
+        [SerializeField][Tooltip("Used to determine movement direction based on input and camera forward axis")] 
+        private Transform playerCamera;
+        [SerializeField][Tooltip("Move speed of the character in")]
+        private float walkSpeed = 3f;
+        [SerializeField][Tooltip("Run speed of the character")] 
+        private float runSpeed = 8f;
+        [SerializeField][Tooltip("The character uses its own gravity value. The engine default is -9.81f")] 
+        private float gravity = -18f;
+        [SerializeField][Tooltip("The height the player can jump ")] 
+        private float jumpHeight = 3f;
 
         private CharacterController controller;
-        private GroundCheck groundCheck;
-
         private GameObject avatar;
-
+        
         private float verticalVelocity;
         private float turnSmoothVelocity;
-        private bool isRunning;
+
         private bool jumpTrigger;
-
-        // 🔑 STATE FLAGS
-        private bool isEquipped;
-        private bool isAiming;
-
         public float CurrentMoveSpeed { get; private set; }
+        private bool isRunning;
 
-        // ===================== UNITY =====================
-
+        private GroundCheck groundCheck;
+        
         private void Awake()
         {
             controller = GetComponent<CharacterController>();
             groundCheck = GetComponent<GroundCheck>();
-
-            if (cameraFollow == null)
-                cameraFollow = FindObjectOfType<CameraFollow>();
         }
 
         public void Setup(GameObject target)
         {
             avatar = target;
+            if (playerCamera == null)
+            {
+                playerCamera = Camera.main.transform;
+            }
         }
 
-        // ===================== PUBLIC API =====================
-
-        public void SetEquipped(bool equipped)
+        public void Move(float inputX, float inputY)
         {
-            isEquipped = equipped;
-            // Intentionally does NOT affect rotation
+            var moveDirection = playerCamera.right * inputX + playerCamera.forward * inputY;
+            var moveSpeed = isRunning ? runSpeed: walkSpeed;
+
+            JumpAndGravity();
+            controller.Move(moveDirection.normalized * (moveSpeed * Time.deltaTime) +  new Vector3(0.0f, verticalVelocity * Time.deltaTime, 0.0f));
+
+            var moveMagnitude = moveDirection.magnitude;
+            CurrentMoveSpeed = isRunning ? runSpeed * moveMagnitude : walkSpeed * moveMagnitude;
+            
+            if (moveMagnitude > 0)
+            {
+                RotateAvatarTowardsMoveDirection(moveDirection);
+            }
         }
 
-        public void SetAiming(bool aiming)
+        private void RotateAvatarTowardsMoveDirection(Vector3 moveDirection)
         {
-            isAiming = aiming;
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + transform.rotation.y;
+            float angle = Mathf.SmoothDampAngle(avatar.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TURN_SMOOTH_TIME);
+            avatar.transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+
+        private void JumpAndGravity()
+        {
+            if (controller.isGrounded && verticalVelocity< 0)
+            {
+                verticalVelocity = -2f;
+            }
+            
+            if (jumpTrigger && controller.isGrounded)
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                jumpTrigger = false;
+            }
+            
+            verticalVelocity += gravity * Time.deltaTime;
         }
 
         public void SetIsRunning(bool running)
         {
             isRunning = running;
         }
-
+        
         public bool TryJump()
         {
-            if (IsGrounded())
+            jumpTrigger = false;
+            if (controller.isGrounded)
             {
                 jumpTrigger = true;
-                return true;
             }
-            return false;
-        }
-
-        // ===================== MOVEMENT =====================
-
-        public void Move(float inputX, float inputY)
-        {
-            if (cameraFollow == null || avatar == null)
-                return;
-
-            Vector3 inputDirection = new Vector3(inputX, 0f, inputY);
-            float moveMagnitude = Mathf.Clamp01(inputDirection.magnitude);
-            float moveSpeed = isRunning ? runSpeed : walkSpeed;
-
-            ApplyGravityAndJump();
-
-            if (moveMagnitude < 0.01f)
-            {
-                controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
-                CurrentMoveSpeed = 0f;
-                return;
-            }
-
-            // Camera-relative movement (ALWAYS)
-            Vector3 moveDirection =
-                Quaternion.Euler(0f, cameraFollow.Yaw, 0f) * inputDirection;
-
-            float controlMultiplier = IsGrounded() ? 1f : airControlMultiplier;
-
-            controller.Move(
-                moveDirection.normalized *
-                moveSpeed *
-                moveMagnitude *
-                controlMultiplier *
-                Time.deltaTime +
-                Vector3.up * verticalVelocity * Time.deltaTime
-            );
-
-            CurrentMoveSpeed = moveSpeed * moveMagnitude;
-
-            HandleRotation(moveDirection);
-        }
-
-        // ===================== ROTATION =====================
-
-        private void HandleRotation(Vector3 moveDirection)
-        {
-            if (moveDirection.sqrMagnitude < 0.001f)
-                return;
-
-            float targetAngle;
-
-            if (isAiming)
-            {
-                // 🎯 Shooting → face camera
-                targetAngle = cameraFollow.Yaw;
-            }
-            else
-            {
-                // 🚶 Normal locomotion → face movement
-                targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-            }
-
-            float smoothAngle = Mathf.SmoothDampAngle(
-                avatar.transform.eulerAngles.y,
-                targetAngle,
-                ref turnSmoothVelocity,
-                TURN_SMOOTH_TIME
-            );
-
-            avatar.transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-        }
-
-        // ===================== GRAVITY & JUMP =====================
-
-        private void ApplyGravityAndJump()
-        {
-            if (IsGrounded() && verticalVelocity < 0f)
-                verticalVelocity = -2f;
-
-            if (jumpTrigger && IsGrounded())
-            {
-                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                jumpTrigger = false;
-            }
-
-            verticalVelocity += gravity * Time.deltaTime;
+            return jumpTrigger;
         }
 
         public bool IsGrounded()
         {
-            return groundCheck.IsGrounded() && verticalVelocity <= 0f;
+            if (verticalVelocity > 0)
+            {
+                return false;
+            }
+            return groundCheck.IsGrounded();
         }
     }
 }
